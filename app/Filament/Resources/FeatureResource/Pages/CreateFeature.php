9dbb28c64afba00a302d19eba597b924cb9d811a
tamer-dr <tamerdaoud247@gmail.com>
2025-10-19 23:59:30 +0100

    feature redirect

--- app/Filament/Resources/FeatureResource/Pages/CreateFeature.php
+++ app/Filament/Resources/FeatureResource/Pages/CreateFeature.php
@@ -9,4 +9,13 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateFeature extends CreateRecord
 {
     protected static string $resource = FeatureResource::class;
-}
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Caractéristique créée avec succès';
+    }
