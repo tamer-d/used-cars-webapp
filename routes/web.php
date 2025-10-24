e4e1be75cf9262aced91f436052e7b14b932f3d9
tamer-dr <tamerdaoud247@gmail.com>
2025-10-24 19:05:01 +0100

    add more routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\ProfileController;
 
 Route::view('/', 'welcome');
 
@@ -14,15 +19,43 @@ Route::view('profile', 'profile')
 
 require __DIR__.'/auth.php';
 
-// Route pour la liste des voitures
+// Routes publiques pour les voitures
 Route::get('/cars', [CarController::class, 'index'])->name('cars.index');
+Route::get('/cars/{car}', [CarController::class, 'show'])->name('cars.show');
 
-// Route pour la page About Us
+// Routes protégées par authentification
+Route::middleware(['auth'])->group(function () {
+    // Routes pour les voitures (utilisateurs connectés)
+    Route::get('/cars/create', [CarController::class, 'create'])->name('cars.create');
+    Route::post('/cars', [CarController::class, 'store'])->name('cars.store');
+    Route::get('/cars/{car}/edit', [CarController::class, 'edit'])->name('cars.edit');
+    Route::put('/cars/{car}', [CarController::class, 'update'])->name('cars.update');
+    Route::delete('/cars/{car}', [CarController::class, 'destroy'])->name('cars.destroy');
+    Route::get('/my-cars', [CarController::class, 'myCars'])->name('cars.my-cars');
+    
+    // Routes pour les favoris
+    Route::get('/favorites', [FavoriteController::class, 'index'])->name('favorites.index');
+    Route::post('/favorites', [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('/favorites/{favorite}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+    
+    // Routes pour les messages
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::post('/messages', [MessageController::class, 'store'])->name('messages.store');
+    Route::get('/messages/{message}', [MessageController::class, 'show'])->name('messages.show');
+    
+    // Routes pour les avis
+    Route::post('/reviews', [ReviewController::class, 'store'])->name('reviews.store');
+    
+    // Routes pour le profil
+    Route::get('/profile/show', [ProfileController::class, 'show'])->name('profile.show');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+});
+
+// Routes pour les pages statiques
 Route::get('/about', function () {
     return view('pages.about');
 })->name('about');
 
-// Route pour la page Contact
 Route::get('/contact', function () {
     return view('pages.contact');
 })->name('contact');
