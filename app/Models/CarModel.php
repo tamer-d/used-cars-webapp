10b27709f0a27c96aabb394d0995d2746a301630
tamer-dr <tamerdaoud247@gmail.com>
2025-10-26 00:28:19 +0100

    add cars model

--- app/Models/CarModel.php
+++ app/Models/CarModel.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class CarModel extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['brand_id', 'name'];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class, 'model_id');
+    }
 }
